d9ee62d38417862db69a65c32443a3957f1de3ef
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-08 18:47:06 +0000

    Positioned elements, added external fonts, positioned the nav, added the header, setup the main container for Tweeter Assignment

---
+++ public/scripts/compose-char-counter.js
@@ -0,0 +1,14 @@
+/*
+Task: Implementthe character counter (Tweeter Assignment)
+*/
+
+
+// Using jQuery and an appropriate selector, register an event handler to th textarea element
+// for the form 
+$(document).ready(function() { // runs a callback when the DOM is ready to be manipulated with jQuery
+  console.log( "ready!" );
+})
+
+$('textarea').keyup(function() {
+  let maxChar = $('#counter');
+})
