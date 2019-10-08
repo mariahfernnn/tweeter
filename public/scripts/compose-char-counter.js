74b44fc5cbbded284fe4ae1c9854e5de8a473a0a
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-08 20:14:54 +0000

    Implementing the character counter - Tweeter Assignment

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -6,9 +6,16 @@ Task: Implementthe character counter (Tweeter Assignment)
 // Using jQuery and an appropriate selector, register an event handler to th textarea element
 // for the form 
 $(document).ready(function() { // runs a callback when the DOM is ready to be manipulated with jQuery
-  console.log( "ready!" );
-})
-
-$('textarea').keyup(function() {
-  let maxChar = $('#counter');
+  $('#textarea').on('keyup', function() {
+    // console.log( "ready!" );
+    let current = $(this).val().length;
+    // let counter = current;
+    
+    if (current <= 140) {
+      $('#counter').text(140 - current);
+    } else if (current > 140) {
+      $('#counter').text('-' + (current - 140));
+      $('#counter').css('color', 'red');
+    }
+  })
 })
