c8f1547f619c38e8f851f7418fbf90779c39feb3
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-09 13:52:36 +0000

    Cleaned up function by removing commented out lines

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -3,13 +3,10 @@ Task: Implementthe character counter (Tweeter Assignment)
 */
 
 
-// Using jQuery and an appropriate selector, register an event handler to th textarea element
-// for the form 
+// Using jQuery and an appropriate selector, register an event handler to th textarea element for the form 
 $(document).ready(function() { // runs a callback when the DOM is ready to be manipulated with jQuery
   $('#textarea').on('keyup', function() {
-    // console.log( "ready!" );
     let current = $(this).val().length;
-    // let counter = current;
     
     if (current <= 140) {
       $('#counter').text(140 - current);
