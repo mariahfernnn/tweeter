1d638cfa6dcac42d99fb65e6dac405a0f44576aa
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-09 16:50:21 +0000

    createTweetElement and renderTweets functions reviewed by mentor

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,73 +9,68 @@
     2. Returns a tweet <article> element containing the entire HTML structure of the tweet
  */
 
-
-
-$(document).ready(function() {
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
     },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
-  const createTweetElement = function(tweetData) {
-    const markup = `
-    <section class="all-tweets" id="hover">
-    <form method="GET" action="/tweets/">
-     <header>
-        <img class="avatar" src="${tweetData.user.avatars}">
-        <span id="name">${tweetData.user.name}</span>
-        <span id="user-handle">${tweetData.user.handle}</span> 
-      </header>
-     <p>${tweetData.content.text}</p>
-      <footer class="footer">
-       <span>${Date(tweetData.created_at)}</span>
-       <i class="fas fa-flag" id="flag"></i>
-       <i class="fas fa-retweet" id="retweet"></i>
-       <i class="fas fa-heart" id="heart"></i>
-     </footer>
-   </form>
-   </section>
+const createTweetElement = function(tweetData) {
+  const markup = `
+  <section class="all-tweets" id="hover">
+<form method="GET" action="/tweets/">
+  <header>
+    <img class="avatar" src="${tweetData.user.avatars}">
+    <span id="name">${tweetData.user.name}</span>
+    <span id="user-handle">${tweetData.user.handle}</span> 
+  </header>
+  <p>${tweetData.content.text}</p>
+  <footer class="footer">
+    <span>${new Date(tweetData.created_at)}</span>
+    <i class="fas fa-flag" id="flag"></i>
+    <i class="fas fa-retweet" id="retweet"></i>
+    <i class="fas fa-heart" id="heart"></i>
+  </footer>
+</form>
+</section>
     `;
-    return markup;
-  }
-  
-  const renderTweets = function(tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and append it to the tweets container
-    for (let tweet of tweets) {
-      let $data = createTweetElement(tweet);
-      $('.container').append($data);
-    }
-  }
-  renderTweets(tweetData);
-});
+  return markup;
+}
 
-// const $data = createTweetElement(tweetData);
-// // Test / driver code (temporary)
-// console.log($data); // to see what it looks like
-// $('.container').append($data); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and append it to the tweets container
+  for (let tweet of tweets) {
+    let $data = createTweetElement(tweet);
+    $('.container').append($data);
+  }
+}
+
+// Load the page then render the data
+$(document).ready(function() {
+renderTweets(tweetData);
+})
 
 
 
