dc9f878244924b5a95d8e5f13e32dae4f0527fe4
Maria Hannah Fernandez <maria.h.fernandez@ryerson.ca>
2019-10-10 19:52:06 +0000

    Displaying Validation Errors With jQuery - replaced alerts with jQuery calls that hide/show the error element

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -3,12 +3,13 @@ Task: Implementthe character counter (Tweeter Assignment)
 */
 
 
-// Using jQuery and an appropriate selector, register an event handler to th textarea element for the form 
+// Using jQuery and an appropriate selector, register an event handler to the textarea element for the form 
 $(document).ready(function() { // runs a callback when the DOM is ready to be manipulated with jQuery
   $('#textarea').on('keyup', function() {
     let current = $(this).val().length;
-    
-    if (current <= 140) {
+    if (current >= 1 || current <= 140) {
+      $('#error-msg-under').slideUp();
+      $('#error-msg-over').slideUp();
       $('#counter').text(140 - current);
       $('#counter').css('color', 'black');
     } else if (current > 140) {
@@ -17,3 +18,5 @@ $(document).ready(function() { // runs a callback when the DOM is ready to be ma
     }
   })
 })
+
+
